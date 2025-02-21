27a80ba833020823d3cfd129fc8ed45cefd1510c
itsujordan <jordan.turvey2@gmail.com>
2025-02-22 10:15:20 +1100

    Update general.js

--- general.js
+++ general.js
@@ -1,4 +1,4 @@
-const pastebinRawUrl = 'https://cors-anywhere.herokuapp.com/https://pastebin.com/raw/nDZ4Juba';
+const pastebinRawUrl = 'https://cors-anywhere.herokuapp.com/https://pastebin.com/raw/8yAWUnbC';
 var buy2 = "LOADING...";
 var CA;
 const twitter = "https://x.com/hotcheetosol";
